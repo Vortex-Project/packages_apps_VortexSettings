c916b3f800c80d9663e4b783ff15b0b6f40625a8
brituj <ritujbeniwal@gmail.com>
2019-07-14 23:26:59 -0600

    Add Screen Stabilization [3/3]

--- src/com/vortex/settings/fragments/MiscSettings.java
+++ src/com/vortex/settings/fragments/MiscSettings.java
@@ -16,13 +16,13 @@
 
 package com.vortex.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.res.Resources;
@@ -31,10 +31,13 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 
 import java.util.Arrays;
@@ -45,18 +48,79 @@ import com.android.settings.SettingsPreferenceFragment;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private ListPreference mVelocityFriction;
+    private ListPreference mPositionFriction;
+    private ListPreference mVelocityAmplitude;
+
+    ContentResolver resolver; 
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.vortex_settings_misc);
 
+        resolver = getActivity().getContentResolver(); 
+
+        float velFriction = Settings.System.getFloatForUser(resolver,
+                    Settings.System.STABILIZATION_VELOCITY_FRICTION,
+                    0.1f,
+                    UserHandle.USER_CURRENT);
+        mVelocityFriction = (ListPreference) findPreference("stabilization_velocity_friction");
+    	mVelocityFriction.setValue(Float.toString(velFriction));
+    	mVelocityFriction.setSummary(mVelocityFriction.getEntry());
+    	mVelocityFriction.setOnPreferenceChangeListener(this);
+
+    	float posFriction = Settings.System.getFloatForUser(resolver,
+                    Settings.System.STABILIZATION_POSITION_FRICTION,
+                    0.1f,
+                    UserHandle.USER_CURRENT);
+            mPositionFriction = (ListPreference) findPreference("stabilization_position_friction");
+    	mPositionFriction.setValue(Float.toString(posFriction));
+    	mPositionFriction.setSummary(mPositionFriction.getEntry());
+    	mPositionFriction.setOnPreferenceChangeListener(this);
+
+    	int velAmplitude = Settings.System.getIntForUser(resolver,
+                    Settings.System.STABILIZATION_VELOCITY_AMPLITUDE,
+                    8000,
+                    UserHandle.USER_CURRENT);
+            mVelocityAmplitude = (ListPreference) findPreference("stabilization_velocity_amplitude");
+    	mVelocityAmplitude.setValue(Integer.toString(velAmplitude));
+    	mVelocityAmplitude.setSummary(mVelocityAmplitude.getEntry());
+    	mVelocityAmplitude.setOnPreferenceChangeListener(this);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) { 
+        final String key = preference.getKey(); 
+        final String setting = getSystemPreferenceString(preference);
+        if (preference != null && preference instanceof ListPreference) {
+               ListPreference listPref = (ListPreference) preference;
+               String value = (String) objValue;
+               int index = listPref.findIndexOfValue(value);
+               listPref.setSummary(listPref.getEntries()[index]);
+    	    listPref.setValue(value);
+    	    if(preference != mVelocityAmplitude){
+    		Settings.System.putFloatForUser(getContentResolver(), setting, Float.valueOf(value),
+    			UserHandle.USER_CURRENT);
+    	    }else {
+    		Settings.System.putIntForUser(getContentResolver(), setting, Integer.valueOf(value),
+    			UserHandle.USER_CURRENT);
+    	    }
+    	}
+        return true; 
+    }
 
-        return false;
+    private String getSystemPreferenceString(Preference preference) {
+        if (preference == null) {
+                return "";
+        } else if(preference == mVelocityFriction){
+            return Settings.System.STABILIZATION_VELOCITY_FRICTION;
+        } else if(preference == mPositionFriction){
+            return Settings.System.STABILIZATION_POSITION_FRICTION;
+        } else if(preference == mVelocityAmplitude){
+            return Settings.System.STABILIZATION_VELOCITY_AMPLITUDE;
+        }  
+        return "";
     }
 
     @Override
